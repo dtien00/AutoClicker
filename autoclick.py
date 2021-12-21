333d4881bb486f9696622b79247032c1896cb43e
dtien00 <61069268+dtien00@users.noreply.github.com>
2021-12-21 11:41:14 -0500

    Version 1.0
    Written in Python
    - Prompt asking for number of clicks
    - Delay before clicking to allow adjustment of cursor
    - Prompt for whether or not to rerun
    - Currently no issues/bugs

---
+++ autoclick.py
@@ -0,0 +1,36 @@
+from easygui.boxes import button_box
+import pyautogui
+import easygui
+import time
+#x coordinate goes left to right as it gets larger
+#y coordinate goes up to down as it gets larger
+def main(iterations): 
+    time.sleep(3)  #Time to move cursor to what you want to click repeatedly
+    cursorX = pyautogui.position().x
+    cursorY = pyautogui.position().y
+    pyautogui.click(cursorX,cursorY, clicks=iterations, interval=0.05)
+
+def rerun():
+    reply = easygui.buttonbox("Rerun?", choices=choices)
+    print(reply)
+    global onRepeat 
+    if (reply=="Yes"): 
+        onRepeat = True
+    else:
+        onRepeat = False
+    print("END")
+
+# easygui.msgbox("Hello World!", title="Autoclicker")
+onRepeat= True #Variable used to determine whether or not to rerun
+msg = "Enter a number of clicks (0-999)"
+title = "Number of clicks"
+choices = ["Yes","No"]
+while(onRepeat==True):
+    print("ITERATION")
+    iterations = easygui.integerbox(msg, title)
+    if(iterations!=None):
+        main(iterations)
+        rerun()
+    else:
+        onRepeat = False
+    
