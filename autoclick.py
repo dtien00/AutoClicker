ac3d846677b4ddac487ba45c67b337757b31c7d8
dtien00 <61069268+dtien00@users.noreply.github.com>
2021-12-28 00:14:59 -0500

    Version 1.1
    Added lower and upper bound editable capability through the variables floor and ceiling respectively. Otherwise default integerbox only has a lower bound of 0, upper bound of 99.

--- autoclick.py
+++ autoclick.py
@@ -1,4 +1,3 @@
-from easygui.boxes import button_box
 import pyautogui
 import easygui
 import time
@@ -8,7 +7,7 @@ def main(iterations):
     time.sleep(3)  #Time to move cursor to what you want to click repeatedly
     cursorX = pyautogui.position().x
     cursorY = pyautogui.position().y
-    pyautogui.click(cursorX,cursorY, clicks=iterations, interval=0.05)
+    pyautogui.click(cursorX,cursorY, clicks=iterations, interval=0.01)
 
 def rerun():
     reply = easygui.buttonbox("Rerun?", choices=choices)
@@ -25,9 +24,11 @@ onRepeat= True #Variable used to determine whether or not to rerun
 msg = "Enter a number of clicks (0-999)"
 title = "Number of clicks"
 choices = ["Yes","No"]
+floor = 0
+ceiling = 999
 while(onRepeat==True):
     print("ITERATION")
-    iterations = easygui.integerbox(msg, title)
+    iterations = easygui.integerbox(msg, title, lowerbound=floor, upperbound=ceiling)
     if(iterations!=None):
         main(iterations)
         rerun()
